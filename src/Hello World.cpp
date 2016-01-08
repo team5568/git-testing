e4c31aad4492e654147ef6e02c9feaaffc1c3889
ksundberg-5568 <kristy.sundberg@outlook.com>
2016-01-08 16:11:12 -0600

    Initial Commit

---
+++ src/Hello World.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	add(5, 7);
+
+	return 0;
+}
+
+
---
+++ src/header.h
@@ -0,0 +1,17 @@
+/*
+ * header.h
+ *
+ *  Created on: Jan 4, 2016
+ *      Author: Family Home User
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+int public add(int y, int z){
+	return y + z;
+}
+
+
+
+#endif /* HEADER_H_ */
